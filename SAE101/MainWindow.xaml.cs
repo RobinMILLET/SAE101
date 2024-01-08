5266892ccd920818359836183efd4217d46a7b55
RobinMILLET <millerob@etu.univ-smb.fr>
2024-01-08 11:41:49 +0100

    Constantes et variables principales

--- SAE101/MainWindow.xaml.cs
+++ SAE101/MainWindow.xaml.cs
@@ -12,11 +12,24 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace SAE101
 {
     public partial class MainWindow : Window
     {
+        // Gestion boutons
+        private bool boutonPressé = false;
+        private Key[] boutonsValides = new Key[] { Key.Z, Key.W, Key.Up, Key.Space, Key.Enter };
+        private Key boutonPerso; // Bouton personnalisable de l'utilisateur
+        // Score et Temps
+        private int score = 0;
+        private int temps = 0;
+        // Moteur
+        private DispatcherTimer Horloge = new DispatcherTimer();
+        private int deltaIPS = 16;
+        private bool pause = true;
+
         public MainWindow()
         {
             InitializeComponent();
